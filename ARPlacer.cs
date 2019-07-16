ada482bf154ab582fbff49ebfe8bbccd126151f8
satyam0751 <44131794+satyam0751@users.noreply.github.com>
2019-07-16 15:10:56 +0530

    Add files via upload

---
+++ ARPlacer.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.Experimental.XR;
+
+
+public class ARPlacer : MonoBehaviour
+{
+    ARSessionOrigin arSessionOrigin;
+
+    List<ARRaycastHit> raycast_hits = new List<ARRaycastHit>();
+
+
+
+    //this is the prefab to be instantiated
+    public GameObject ARObjectPrefab;
+
+    //this is the gameobject that is instantiated after successful raycast intersction with a plane
+    private GameObject spawnedARObject; 
+
+
+
+
+    private void Awake()
+    {
+        arSessionOrigin = GetComponent<ARSessionOrigin>();
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.touchCount > 0)
+        {
+
+            Touch touch = Input.GetTouch(0);
+
+            if (arSessionOrigin.Raycast(touch.position, raycast_hits, TrackableType.PlaneWithinPolygon))
+            {
+                //getting the pose of the hit
+                Pose pose = raycast_hits[0].pose;
+
+                if (spawnedARObject == null)
+                {
+                    spawnedARObject = Instantiate(ARObjectPrefab, pose.position, Quaternion.Euler(0, 0, 0));
+                   
+                }
+                else
+                {
+                    spawnedARObject.transform.position = pose.position;
+                    
+                }
+
+            }
+
+
+
+        }
+
+    }
+}
---
+++ AutoPlaceItem.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.XR;
+using UnityEngine.XR.ARFoundation;
+
+public class AutoPlaceItem : MonoBehaviour {
+
+    public GameObject GameObjectToPlace;
+
+    ARSessionOrigin m_SessionOrigin;
+
+    static List<ARRaycastHit> s_Hits = new List<ARRaycastHit>();
+
+    void Awake()
+    {
+        m_SessionOrigin = GetComponent<ARSessionOrigin>();
+    }
+
+    void Update()
+    {
+           
+
+        if (m_SessionOrigin.Raycast(Camera.main.ViewportPointToRay(new Vector3(0.5f,0.5f,0)), s_Hits, TrackableType.PlaneWithinPolygon))
+        {
+            Pose hitPose = s_Hits[0].pose;
+            GameObjectToPlace.transform.position = hitPose.position;
+            if (!GameObjectToPlace.activeSelf)
+            {
+                GameObjectToPlace.SetActive(true);
+            }
+        }
+
+    }
+}
